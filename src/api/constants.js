442671384dc278d675da0961d938eb2efcfc9eac
Marco <124935811+MarcoB2003@users.noreply.github.com>
2024-11-07 23:02:56 -0300

    Update constants.js

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,2 +1,2 @@
-//export const API_URL = 'http://localhost:5001';
-export const API_URL = 'http://13.91.135.55:3000';
+export const API_URL = 'http://localhost:5001';
+//export const API_URL = 'http://13.91.135.55:5001';
